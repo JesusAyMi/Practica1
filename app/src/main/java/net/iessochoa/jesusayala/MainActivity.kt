15fce7fa9f7ac7c8f5fa0236716b277cfea294ac
JesusAyala <jesusayalamilan2@gmail.com>
2022-10-30 21:26:17 +0100

    Paso 46

--- app/src/main/java/net/iessochoa/jesusayala/MainActivity.kt
+++ app/src/main/java/net/iessochoa/jesusayala/MainActivity.kt
@@ -1,11 +1,35 @@
 package net.iessochoa.jesusayala
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etNombre: EditText
+    private lateinit var btAceptar: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etNombre = findViewById(R.id.etNombre)
+        btAceptar = findViewById(R.id.btAceptar)
+
+        btAceptar.setOnClickListener{enviarSaludo()}
     }
+
+    fun enviarSaludo(){
+        if (!etNombre.text.isEmpty()) {
+            val intent = Intent(this, SaludoActivity::class.java)
+            intent.putExtra(SaludoActivity.EXTRA_NOMBRE, etNombre.text.toString())
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "Es necesario introducir el nombre", Toast.LENGTH_LONG).show()
+        }
+    }
+
 }
---
+++ app/src/main/java/net/iessochoa/jesusayala/SaludoActivity.kt
@@ -0,0 +1,23 @@
+package net.iessochoa.jesusayala
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class SaludoActivity : AppCompatActivity() {
+
+    private lateinit var tvSaludo: TextView
+
+    companion object{
+        val EXTRA_NOMBRE = "practica1.SaludoActivity.nombre"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_saludo)
+
+        tvSaludo=findViewById(R.id.tvSaludo)
+        val nombre=intent.getStringExtra(EXTRA_NOMBRE)
+        tvSaludo.text="Hola "+nombre+"!!"
+    }
+}
